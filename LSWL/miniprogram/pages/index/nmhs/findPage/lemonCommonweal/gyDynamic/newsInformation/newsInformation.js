def93ce640a28b78be2d73c58a41800086e08a96
lingfe <13068326391@163.com>
2020-09-01 12:05:38 +0800

    add Signed-off-by: lingfe <13068326391@163.com>

---
+++ LSWL/miniprogram/pages/index/nmhs/findPage/lemonCommonweal/gyDynamic/newsInformation/newsInformation.js
@@ -0,0 +1,109 @@
+/**  
+ *   作者:  lingfe 
+ *   时间:  2017-11-29
+ *   描述:  新闻详细
+ * 
+ * */
+var app = getApp();
+
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+  
+  },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    var that = this;
+
+    //柠檬公益动态表,单条
+    that.getlemonDynamicId(that, options.id);
+  },
+
+  //柠檬公益动态表,单条
+  getlemonDynamicId: function (that,id) {
+    var url = app.config.basePath_web + "api/exe/get";
+    //请求头
+    var header = { cookie: wx.getStorageSync("cookie"), "Content-Type": "application/x-www-form-urlencoded" };
+    //参数
+    var data = {
+      timeStamp: wx.getStorageSync("time"),
+      token: wx.getStorageSync("token"),
+      reqJson: JSON.stringify({
+        nameSpace: 'lemonDynamic',           //柠檬公益动态表
+        scriptName: 'Query',
+        nameSpaceMap: {
+          rows: [{
+            id:id,
+            state: 0,  //状态，0=正常  
+          }]
+        }
+      })
+    };
+
+    //发送请求
+    app.request.reqPost(url, header, data, function (res) {
+      console.log(res);
+      //验证是否为空如果为空就生成一条贡献
+      if (!app.checkInput(res.data.rows)) {
+        var rows = res.data.rows[0];
+        that.setData({
+          lemonDynamic: rows,
+        });
+      }
+    });
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+  
+  }
+})
